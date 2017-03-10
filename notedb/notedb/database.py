7a70c54fadd1c65000eaae75a024fc76f3985316
jayates <jayates@jayates-MOBL1.amr.corp.intel.com>
2017-03-09 17:29:42 -0800

    First crack at sqlalchemy

---
+++ notedb/notedb/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+user = 'root'
+password = 'bassfish'
+host = 'localhost'
+port = '3306'
+database = 'dev_notedb'
+engine = create_engine('mysql://root:bassfish@localhost/dev_notedb', echo=True)
+
+Base = declarative_base()
+
+class Note(Base):
+    __tablename__ = 'notes'
+    
+    id = Column(Integer, primary_key=True)
+    
+Base.metadata.create_all(engine)
