c98785e8b1d2b6edb64920ad25dc323ec64a529f
jayates <jayates@jayates-MOBL1.amr.corp.intel.com>
2017-03-14 09:29:42 -0700

    Added some logging and started argparsing in main

--- notedb/notedb/main.py
+++ notedb/notedb/main.py
@@ -1,7 +1,42 @@
+import argparse
 import configparser
+import logging
+
+
+from notedb.tape import TapeParser
 
 def main():
-    pass
+    args = get_cmdline_arguments()
+    init_logging(args)
+    
+    tape_parser = TapeParser(args.input_tape)
+    
+    if args.output_tape:
+        tape_parser.write_tape_xls(args.output_tape)
+
+def get_cmdline_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input-tape', required=True, type=argparse.FileType('w'),
+                        help='Input tape xls file')
+    parser.add_argument('-o', '--output-tape', type=argparse.FileType('w'),
+                        help='Write formatted tape xls here')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Increase logging verbosity')
+    parser.add_argument('-l', '--log-file', type=argparse.FileType('w'), help='Output log file')
+    parser.add_argument('--format-csv', help='Tape formatting CSV', type=argparse.FileType('r'))
+    parser.add_argument('--format-name', help='Tape format name within CSV')
+    return parser.parse_args()
+
+def init_logging(args):
+    level = logging.INFO
+    if args.verbose:
+        level = logging.DEBUG
+        
+    log_format = '%(levelname)s: %(message)s'
+    
+    if (args.log_file):
+        logging.basicConfig(level=level, format=log_format, filename=args.log_file, filemode='w')
+    else:
+        logging.basicConfig(level=level, format=log_format)
 
 def parse_config_file(config_file):
         config = configparser.ConfigParser()
--- notedb/notedb/tape.py
+++ notedb/notedb/tape.py
@@ -2,7 +2,9 @@ from sqlalchemy import Column, Integer, Table, ForeignKey
 from sqlalchemy.orm import relationship
 from notedb.database import Base
 
+import logging
 import pandas as pd
+import sys
     
 tapes_notes_table = Table(
     'association', Base.metadata,
@@ -25,23 +27,28 @@ class TapeParser:
     
     def __init__(self, input_tape_xls):
         self.tape_df = pd.read_excel(input_tape_xls)
+        logging.info('Tape parsed from %s', input_tape_xls)
         
     def write_tape_xls(self, output_tape_xls):
         writer = pd.ExcelWriter(output_tape_xls)
         self.tape_df.to_excel(writer)
         writer.save()
+        logging.info('Tape written to file %s', output_tape_xls)
         
     def format_tape_columns(self, csv, format_name):
         tape_formats_df = pd.read_csv(csv)
         tape_format = self._get_tape_format(tape_formats_df, format_name)
         self._rename_or_add_columns(tape_format)
         self._reorder_columns(tape_format.keys())
+        logging.info('Tape column formatting completed')
         
     def _get_tape_format(self, formats_df, format_name):      
         for index, row in formats_df.iterrows():
             if row[self.FORMAT_NAME_KEY] == format_name:
                 del row[self.FORMAT_NAME_KEY] # Non-Data column
                 return row # Choose first matching row
+        logging.error('Column format "%s" not found')
+        sys.exit()
         
     def _rename_or_add_columns(self, tape_format):
         """Rename tape columns if a mapping exists, otherwise create new column."""
@@ -50,8 +57,11 @@ class TapeParser:
                 try:
                     self.tape_df[new_name] = self.tape_df[old_name]
                     del self.tape_df[old_name]
-                except (KeyError, ValueError):
+                    logging.info('Tape column "%s" renamed to "%s"', old_name, new_name)
+                except (KeyError, ValueError): # old_name column doesn't exist
                     self.tape_df[new_name] = ''
+                    logging.info('New tape column "%s" created', new_name)
                     
     def _reorder_columns(self, column_order):
         self.tape_df = self.tape_df[column_order]
+        logging.info('Tape columns reordered')
