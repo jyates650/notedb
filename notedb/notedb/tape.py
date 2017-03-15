60722222e54a80855204ec5fcd881b332c98279f
jayates <jayates@jayates-MOBL1.amr.corp.intel.com>
2017-03-15 11:57:50 -0700

    Added the TapeWriter class and added some docstrings

--- notedb/notedb/tape.py
+++ notedb/notedb/tape.py
@@ -4,24 +4,38 @@ import sys
 from openpyxl import load_workbook
 
 class TapeParser:
+    """Responsible for parsing a raw Tape xls file into a Tape object"""
     
     FORMAT_NAME_KEY = 'TapeFormat'
     
-    def __init__(self, input_tape_xls):
-        self.tape_df = pandas.read_excel(input_tape_xls)
-        logging.info('Tape parsed from %s', input_tape_xls)
+    def __init__(self):
+        self.tape_df = None
         
-    def get_tape_object(self):
-        return Tape(self.tape_df)
+    def parse_tape(self, tape_xls):
+        """Read the tape xls and save as a DataFrame"""
+        self.tape_df = pandas.read_excel(tape_xls)
+        logging.info('Tape data parsed from %s', tape_xls)
         
     def format_tape_columns(self, csv, format_name):
+        """Change the tape DataFrame columns according to the format specified in the csv.
+        
+        The first row of the CSV contains the desired column names in the desired order.
+        
+        The first column of the CSV is the format_name label, used to specify a format for a
+        given Tape. This column is not included when mapping names.
+        """
         tape_formats_df = pandas.read_csv(csv)
         tape_format = self._get_tape_format(tape_formats_df, format_name)
         self._rename_or_add_columns(tape_format)
         self._reorder_columns(tape_format.keys())
         logging.info('Tape column formatting completed')
         
-    def _get_tape_format(self, formats_df, format_name):      
+    def get_tape_object(self):
+        """Return the Tape object created from the parsed tape"""
+        return Tape(self.tape_df)
+           
+    def _get_tape_format(self, formats_df, format_name):    
+        """Return the series of header mappings specified in the formats_df by the format_name"""  
         for index, row in formats_df.iterrows():
             if row[self.FORMAT_NAME_KEY] == format_name:
                 del row[self.FORMAT_NAME_KEY] # Non-Data column
@@ -42,42 +56,66 @@ class TapeParser:
                     logging.info('New tape column "%s" created', new_name)
                     
     def _reorder_columns(self, column_order):
+        """Reorder the Tape DataFrame columns to match the column order provided"""
         self.tape_df = self.tape_df[column_order]
         logging.info('Tape columns reordered')
 
 
+class TapeWriter:
+    """Responsible for writing Tape data to an excel file, with support for an output template"""
+    
+    def __init__(self):
+        self.tape_df = None
+        self.template_df = None
+        self.writer = None
+        
+    def write_xls(self, tape_df, output_xls, template_xls=None):
+        """Write Tape DataFrame to excel file. Use excel output template if provided"""
+        self.tape_df = tape_df
+        self.writer = pandas.ExcelWriter(output_xls)
+        
+        if template_xls:
+            self._parse_template(template_xls)
+            self._populate_template_with_tape_data()
+            self._write_dataframe_to_xls(self.template_df, output_xls)
+        else:
+            self._write_dataframe_to_xls(self.tape_df, output_xls)
+        
+    def _parse_template(self, template_xls):
+        """Read template xls. Save the data and sheets in TapeWriter"""
+        # TODO add try/except block
+        self.template_df = pandas.read_excel(template_xls)
+        logging.info('Loaded template: %s', template_xls)
+        
+        # Make sure other sheets in the template are copied
+        book = load_workbook(template_xls)
+        self.writer.book = book
+        self.writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+        
+    def _populate_template_with_tape_data(self):
+        """Populate the template DataFrame with matching columns in the Tape DF"""
+        # TODO add try/except block
+        for col in self.template_df.columns:
+            self.template_df[col] = self.tape_df[col]
+        logging.info('Template populated with Tape data')
+        
+    def _write_dataframe_to_xls(self, dataframe, output_xls):
+        """Write the DataFrame to an excel file"""
+        # TODO add try/except block
+        dataframe.to_excel(self.writer, index=False) # Don't write the index column
+        self.writer.save()
+        logging.info('Tape written to file %s', output_xls)
+        
+
 class Tape:
+    """Responsible for storing Tape Data"""
     
     def __init__(self, dataframe):
         self.dataframe = dataframe
             
     def write_xls(self, output_xls, template_xls=None):
-        """Write Tape data to excel file. Populate data into template if provided"""
-        if template_xls:
-            self._write_xls_with_template(output_xls, template_xls)
-        else:
-            self._write_xls_without_template(output_xls)
-        logging.info('Tape written to file %s', output_xls)
-        
-    def _write_xls_with_template(self, output_xls, template_xls):
-        writer = pandas.ExcelWriter(output_xls)
-        
-        # Read in the template and populate with Tape data
-        template_df = pandas.read_excel(template_xls)
-        for col in self.dataframe.columns:
-            template_df[col] = self.dataframe[col]
-        
-        # Make sure other sheets in the template don't get wiped out
-        book = load_workbook(template_xls)
-        writer.book = book
-        writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
-        
-        # Save the populated template DataFrame to the output file
-        template_df.to_excel(writer, index=False)
-        writer.save()
-    
-    def _write_xls_without_template(self, output_xls):
-        writer = pandas.ExcelWriter(output_xls)
-        self.dataframe.to_excel(writer, index=False)
-        writer.save()
+        """Write Tape DataFrame to excel file. Use excel output template if provided"""
+        tape_writer = TapeWriter()
+        tape_writer.write_xls(self.dataframe, output_xls, template_xls)
+
         
--- notedb/tests/test_tape.py
+++ notedb/tests/test_tape.py
@@ -20,7 +20,8 @@ class TestTapeParser:
         shutil.rmtree(self.temp_dir)
         
     def test_excel_parsing(self):
-        tape_parser = TapeParser(self.simple_test_tape_xlsx)
+        tape_parser = TapeParser()
+        tape_parser.parse_tape(self.simple_test_tape_xlsx)
         df = tape_parser.tape_df
         assert df['TestHeader1'][0] == 'TestData11'
         assert df['TestHeader1'][1] == 'TestData21'
@@ -28,13 +29,15 @@ class TestTapeParser:
         assert df['TestHeader2'][1] == 'TestData22'
         
     def test_pandas_rename_column(self):
-        tape_parser = TapeParser(self.simple_test_tape_xlsx)
+        tape_parser = TapeParser()
+        tape_parser.parse_tape(self.simple_test_tape_xlsx)
         df = tape_parser.tape_df
         df.rename(columns={'TestHeader1': 'Header1'}, inplace=True)
         assert df['Header1'][0] == 'TestData11'
     
     def test_format_tape_columns(self):
-        tape_parser = TapeParser(self.test_tape_format_1_xlsx)
+        tape_parser = TapeParser()
+        tape_parser.parse_tape(self.test_tape_format_1_xlsx)
         df = tape_parser.tape_df
         assert df['City'][0] == 'Methuen'
         assert len(df.columns) == 5
@@ -45,7 +48,8 @@ class TestTapeParser:
         assert len(df.columns) == 6
         
     def test_reorder_tape_columns(self):
-        tape_parser = TapeParser(self.simple_test_tape_xlsx)
+        tape_parser = TapeParser()
+        tape_parser.parse_tape(self.simple_test_tape_xlsx)
         df = tape_parser.tape_df
         df_cols = df.columns.tolist()
         assert df_cols == ['TestHeader1', 'TestHeader2']
@@ -54,17 +58,20 @@ class TestTapeParser:
         assert tape_parser.tape_df.columns.tolist() == new_order
         
     def test_excel_writing(self):
-        tape_parser = TapeParser(self.simple_test_tape_xlsx)
+        tape_parser = TapeParser()
+        tape_parser.parse_tape(self.simple_test_tape_xlsx)
         df1 = tape_parser.tape_df
         out_file = os.path.join(self.temp_dir, 'outfile1.xlsx')
         tape = tape_parser.get_tape_object()
         tape.write_xls(out_file)
-        tp2 = TapeParser(out_file)
+        tp2 = TapeParser()
+        tp2.parse_tape(out_file)
         df2 = tp2.tape_df
         assert df1['TestHeader1'][1] == df2['TestHeader1'][1]
         
     def test_rename_and_reorder_tape_cols(self):
-        tape_parser = TapeParser(self.test_tape_format_1_xlsx)
+        tape_parser = TapeParser()
+        tape_parser.parse_tape(self.test_tape_format_1_xlsx)
         cols = tape_parser.tape_df.columns.tolist()
         assert cols == ['State', 'Address', 'City', 'Zip', 'BPO']
         tape_parser.format_tape_columns(self.tape_headers_csv, 'format1')
@@ -72,7 +79,8 @@ class TestTapeParser:
         assert cols == ['PropAddr', 'PropCity', 'PropState', 'PropZip', 'BPO', 'Comment']
         
     def test_template_populating(self):
-        tape_parser = TapeParser(self.test_tape_format_1_xlsx)
+        tape_parser = TapeParser()
+        tape_parser.parse_tape(self.test_tape_format_1_xlsx)
         cols = ['State', 'Address', 'City', 'Zip', 'BPO']
         assert tape_parser.tape_df.columns.tolist() == cols
         tape_parser.format_tape_columns(self.tape_headers_csv, 'format1')
