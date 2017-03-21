54a40d5c786bc80ba3eded62f84d96670985a67d
jayates <jayates@jayates-MOBL1.amr.corp.intel.com>
2017-03-21 14:01:14 -0700

    Changed query string to maintain term order. Added tests.

--- notedb/notedb/google.py
+++ notedb/notedb/google.py
@@ -11,9 +11,9 @@ def get_google_url(path, search_terms, website=None):
     """Return Google search URL for the given search terms and optional website.
     path for google maps is '/maps' and for search it is '/search'
     """
-    query = {'q': search_terms}
+    query = [('q', search_terms)] # Use a list of tuple pairs to maintain the order of terms
     if website:
-        query['sitesearch'] = website
+        query.append(('sitesearch', website))
     encoded_query = urlencode(query)
     url_components = (SCHEME, NETLOC, path, PARAMS, encoded_query, FRAGMENT)
     return urlunparse(url_components)
